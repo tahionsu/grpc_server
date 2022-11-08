ea9d6b218a302d8a599c1c4871c9b7a19468e109
Anton Milov <avm@gldva.ru>
2022-11-08 11:15:34 +0700

    Revision #2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,25 +2,24 @@ package org.example;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.example.server.ServerBean;
 import org.example.service.implementation.CoffeeServiceImpl;
 import org.example.template.CoffeeClient;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
-        RestTemplate restTemplate = new RestTemplate();
-        CoffeeClient coffeeClient = new CoffeeClient(restTemplate);
-        Server server = ServerBuilder.forPort(8081)
-                .addService(new CoffeeServiceImpl(coffeeClient))
-                .build();
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(ServerBean.class);
+        ctx.refresh();
 
-        server.start();
+        ServerBean serverBean = ctx.getBean(ServerBean.class);
 
+        serverBean.start();
         System.out.println("Server started");
-
-        server.awaitTermination();
     }
 
 }
---
+++ src/main/java/org/example/server/ServerBean.java
@@ -0,0 +1,34 @@
+package org.example.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.example.service.implementation.CoffeeServiceImpl;
+import org.example.template.CoffeeClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+@Configuration
+public class ServerBean {
+
+    private RestTemplate restTemplate;
+    private CoffeeClient coffeeClient;
+    private Server server;
+
+    public ServerBean() {
+        this.restTemplate = new RestTemplate();
+        this.coffeeClient = new CoffeeClient(restTemplate);
+        this.server = ServerBuilder.forPort(8081)
+                .addService(new CoffeeServiceImpl(coffeeClient))
+                .build();
+    }
+
+    @Bean
+    public void start() throws IOException, InterruptedException {
+        this.server.start();
+        this.server.awaitTermination();
+    }
+
+}
